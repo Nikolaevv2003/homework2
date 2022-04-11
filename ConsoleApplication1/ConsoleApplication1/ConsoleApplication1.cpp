8a585c617e2309db8496bfe3d618739bc8ee04be
Nikolaevv2003 <92081840+Nikolaevv2003@users.noreply.github.com>
2022-04-11 20:52:17 +0300

    Update ConsoleApplication1.cpp

--- ConsoleApplication1/ConsoleApplication1/ConsoleApplication1.cpp
+++ ConsoleApplication1/ConsoleApplication1/ConsoleApplication1.cpp
@@ -1,27 +1,26 @@
-﻿#include <iostream>
+#include <iostream>
 #include <cmath>
 
 using namespace std;
 
 int main()
 {
-    double r1, r2, l, h, v, s;
+    float r1, r2, l, h, v, s;
     cout << "enter R: ";
     cin >> r1;
     cout << "enter r: ";
     cin >> r2;
-    cout << "enter l: ";
-    cin >> l;
     cout << "enter h: ";
     cin >> h;
-    if (l != sqrt((r1/2 - r2/2) * (r1/2 - r2/2) + h * h)) {
-        cout << "error" << endl;
-    }
-    else {
-        v = (3.14 * h * (r1 * r1 + r1 * r2 + r2 * r2)) / 3;
+    if ((r1 > 0) && (r2 > 0) && (h > 0)) {
+        l = sqrt((r1 - r2) * (r1 - r2) + h * h);
+        v = (3.14 * h * (r1 * r1 + r1 * r2 + r2 * r2))/3.0;
         cout << "V = " << v << endl;
         s = 3.14 * (r1 * r1 + l * (r1 + r2) + r2 * r2);
         cout << "S = " << s << endl;
+    }
+    else {
+        cout << "error" << endl;
     }
     return 0;
 }
